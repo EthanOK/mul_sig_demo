dc1c5d3476a54d915f66233918ee093e368162e8
DJ.Zhang <dengji@evg.co>
2024-09-04 15:02:25 +0800

    modified:   README.md

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Solana 离线签名
 
+## partial sign
+
+[RecentBlockhash](https://solana.com/docs/core/transactions#recent-blockhash)
+
+交易的区块哈希有效期为 150 个区块，1 个区块通常会在 400-600ms 间波动，所以交易的区块 hash 的过期时间一般在 60-90 秒。
+
+RecentBlockhash 对于交易来说是一个重要的值。如果您使用最近过期的区块哈希（150 个区块之后），您的交易将被拒绝.
+
 [部分签署交易](https://solanacookbook.com/references/offline-transactions.html#partial-sign-transaction)
+
+## Durable Nonce
+
+使用 `Durable Nonce` 来获取一个永不过期的最近区块哈希。
+
+> Create Nonce Account
+
+> 1. 使用存储在 nonce 账户中的 nonce 作为最近的区块哈希。
+
+> 2. 将 nonce advance 操作放在第一个指令中。
+
+[durable-nonce](https://solanacookbook.com/references/offline-transactions.html#durable-nonce)
--- tests/mul_sig_demo.ts
+++ tests/mul_sig_demo.ts
@@ -1,7 +1,14 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MulSigDemo } from "../target/types/mul_sig_demo";
-import { Transaction, PublicKey, Keypair } from "@solana/web3.js";
+import {
+  Transaction,
+  PublicKey,
+  Keypair,
+  SystemProgram,
+  NONCE_ACCOUNT_LENGTH,
+  NonceAccount,
+} from "@solana/web3.js";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import { backSigner_publicKey, sendBack } from "./back";
 
@@ -9,12 +16,22 @@ describe("mul_sig_demo", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const wallet = anchor.AnchorProvider.env().wallet;
+
   const program = anchor.workspace.MulSigDemo as Program<MulSigDemo>;
 
   const provider = anchor.getProvider();
 
-  it("Is mul sig", async () => {
-    // Add your test here.
+  const payer = Keypair.generate();
+
+  const nonceAccount = Keypair.generate();
+
+  it("Is requestAirdrop!", async () => {
+    const s = await provider.connection.requestAirdrop(payer.publicKey, 10e9);
+    await provider.connection.confirmTransaction(s);
+  });
+
+  it("Is off-chain signature: latestBlockhash", async () => {
     const { blockhash } = await provider.connection.getLatestBlockhash();
 
     const transaction = await program.methods
@@ -37,10 +54,111 @@ describe("mul_sig_demo", () => {
       bs58.encode(serializedTransaction)
     );
 
-    const transaction__backSign = Transaction.from(
+    let transaction__backSign = Transaction.from(
       bs58.decode(serializedTransaction_backSign)
     );
-    const tx = await provider.sendAndConfirm(transaction__backSign);
+
+    transaction__backSign = await wallet.signTransaction(transaction__backSign);
+
+    // if payer is Keypair
+    // transaction__backSign.partialSign(payer);
+
+    // if sleep 100 s, Transaction simulation failed: Blockhash not found.
+    // await new Promise((resolve) => setTimeout(resolve, 100000));
+
+    const tx = await provider.connection.sendRawTransaction(
+      transaction__backSign.serialize()
+    );
     console.log("tx:", tx);
+  });
+
+  it("Is off-chain signature: Durable Nonce: Create Nonce Account", async () => {
+    let transaction = new Transaction().add(
+      // create nonce account
+      SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: nonceAccount.publicKey,
+        lamports: await provider.connection.getMinimumBalanceForRentExemption(
+          NONCE_ACCOUNT_LENGTH
+        ),
+        space: NONCE_ACCOUNT_LENGTH,
+        programId: SystemProgram.programId,
+      }),
+      // init nonce account
+      SystemProgram.nonceInitialize({
+        noncePubkey: nonceAccount.publicKey, // nonce account pubkey
+        authorizedPubkey: payer.publicKey, // nonce account authority (for advance and close)
+      })
+    );
+
+    const txhash = await provider.connection.sendTransaction(transaction, [
+      payer,
+      nonceAccount,
+    ]);
+
+    await provider.connection.confirmTransaction(txhash);
+
+    console.log(`txhash: ${txhash}`);
+  });
+
+  it("Is off-chain signature: Durable Nonce", async () => {
+    const accountInfo = await provider.connection.getAccountInfo(
+      nonceAccount.publicKey
+    );
+    const nonceAccount_parsed = NonceAccount.fromAccountData(accountInfo.data);
+
+    console.log(nonceAccount_parsed);
+
+    const transaction = await program.methods
+      .initialize()
+      .accountsPartial({
+        payer: payer.publicKey,
+        backSigner: backSigner_publicKey,
+      })
+      .preInstructions([
+        SystemProgram.nonceAdvance({
+          noncePubkey: nonceAccount.publicKey,
+          authorizedPubkey: payer.publicKey,
+        }),
+      ])
+      .transaction();
+    transaction.feePayer = payer.publicKey;
+    transaction.recentBlockhash = nonceAccount_parsed.nonce;
+
+    const serializedTransaction = transaction.serialize({
+      requireAllSignatures: false,
+    });
+
+    // send transaction to back get signer transaction
+    const serializedTransaction_backSign = await sendBack(
+      payer.publicKey.toBase58(),
+      bs58.encode(serializedTransaction)
+    );
+
+    let transaction__backSign = Transaction.from(
+      bs58.decode(serializedTransaction_backSign)
+    );
+
+    // transaction__backSign = await wallet.signTransaction(transaction__backSign);
+
+    transaction__backSign.partialSign(payer);
+
+    // console.log("transaction__backSign:", transaction__backSign);
+
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+
+    const tx = await provider.connection.sendRawTransaction(
+      transaction__backSign.serialize()
+    );
+    await provider.connection.confirmTransaction(tx);
+    console.log("tx:", tx);
+
+
+    const accountInfo_after = await provider.connection.getAccountInfo(
+      nonceAccount.publicKey
+    );
+    const nonceAccount_parsed_after = NonceAccount.fromAccountData(accountInfo_after.data);
+
+    console.log(nonceAccount_parsed_after);
   });
 });
